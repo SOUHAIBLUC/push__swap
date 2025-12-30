3c8f61ca3e4c7a6b7752d02dfc9f46913327c469
Souhaib Ait Lahcine <so-ait-l@student.1337.ma>
2025-12-30 14:57:47 +0100

    fixed the main

--- main.c
+++ main.c
@@ -1,57 +1,71 @@
 #include "push_swap.h"
+#include <limits.h>
 #include <stdio.h>
-int ft_atoi(const char *str) {
-  int i = 0;
-  int sign = 1;
-  long result = 0;
 
-  while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
-    i++;
-  if (str[i] == '-' || str[i] == '+') {
-    if (str[i] == '-')
-      sign = -1;
-    i++;
-  }
-  while (str[i] >= '0' && str[i] <= '9') {
-    result = result * 10 + (str[i] - '0');
-    if (result * sign > 2147483647)
-      return (-1);
-    if (result * sign < -2147483648)
-      return (0);
-    i++;
-  }
-  return ((int)(result * sign));
+long ft_atoi(const char *str)
+{
+    int  i      = 0;
+    int  sign   = 1;
+    long result = 0;
+
+    while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+        i++;
+    if (str[i] == '-' || str[i] == '+')
+    {
+        if (str[i] == '-')
+            sign = -1;
+        i++;
+    }
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        result = result * 10 + (str[i] - '0');
+        if (result * sign > 2147483647)
+            return LONG_MAX;
+        if (result * sign < -2147483648)
+            return LONG_MAX;
+        i++;
+    }
+    return (result * sign);
 }
 
-void ft_stack_print(t_node *head) {
-  while (head != NULL) {
-    printf("%d -> ", head->value);
-    head = head->next;
-  }
-  printf("NULL\n");
+void ft_stack_print(t_stack *stack)
+{
+	t_node *tmp;
+	tmp = stack->head;
+    while (tmp != NULL)
+    {
+        printf("%d -> ", tmp->value);
+		tmp = tmp->next;
+    }
+    printf("NULL\n");
 }
 
-int main(int argc, char **argv) {
-  if (argc < 2)
-    return (0);
-  int i = 1;
-  int j = 0;
-  while (argv[i] && argv[i][j]) {
-    if ((argv[i][j] < '0' || argv[i][j] > '9') && argv[i][j] != '-' &&
-        argv[i][j] != '+' && argv[i][j] != ' ') {
-      write(2, "Error\n", 6);
-      return (1);
+
+
+void ft_game_init(t_game *game, int argc, char **argv)
+{
+	if (argc < 2)
+        return ;
+    int i = 1;
+    while (i < argc)
+    {
+		long check;
+		check = ft_atoi(argv[i]);
+		if (check == LONG_MAX)
+		{
+			write(2, "Error\n", 6);
+			stack_clear(&game->a);
+			stack_clear(&game->b);
+		}
+		
+		t_node *node = (ft_atoi(argv[i]));
+		stack_push(&game->a, &node);
     }
-    j++;
-    if (argv[i][j] == '\0') {
-      i++;
-      j = 0;
-    }
-  }
-  i = 1;
-  // j = 0;
-  while (argv[i]) {
-    node_new(ft_atoi(argv[i]));
-    i++;
-  }
+}
+
+int main(int argc, char **argv)
+{
+	t_game game;
+   ft_game_init(&game.a, argc, argv);
+   ft_stack_print(&game.a);
 }
